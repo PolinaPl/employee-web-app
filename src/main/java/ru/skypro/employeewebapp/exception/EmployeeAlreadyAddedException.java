8c63c7af3cba93eef217abdfb6c1a7a2e38271c4
PolinaPl <ppluhina0@gmail.com>
2024-03-12 13:30:59 +0700

    Домашняя работа доработана

--- src/main/java/ru/skypro/employeewebapp/exception/EmployeeAlreadyAddedException.java
+++ src/main/java/ru/skypro/employeewebapp/exception/EmployeeAlreadyAddedException.java
@@ -1,5 +1,9 @@
 package ru.skypro.employeewebapp.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus (code = HttpStatus.BAD_REQUEST)
 public class EmployeeAlreadyAddedException extends RuntimeException{
     public EmployeeAlreadyAddedException() {
         super("Cотрудник уже существует");
